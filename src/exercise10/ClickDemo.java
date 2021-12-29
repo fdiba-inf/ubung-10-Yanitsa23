7d2cdf9a780e50b52d5b4973e30df9c3ebf1c538
Yanitsa23 <>
2021-12-29 21:44:49 +0000

    Some message

--- src/exercise10/ClickDemo.java
+++ src/exercise10/ClickDemo.java
@@ -4,16 +4,16 @@ public class ClickDemo {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle();
         rectangle.initialize();
-
+        
         Ellipse ellipse = new Ellipse();
         ellipse.initialize();
-
+        
         Point clickPoint = new Point();
         clickPoint.initialize();
 
         ClickListener[] clickListeners = new ClickListener[2];
         clickListeners[0] = rectangle;
-        clickListeners[1] = (ClickListener) ellipse; // Cast should be removed after Ellipse implements ClickListener
+        clickListeners[1] = ellipse;
 
         for (int i = 0; i < clickListeners.length; i++) {
             boolean pointInside = clickListeners[i].containsClick(clickPoint);
